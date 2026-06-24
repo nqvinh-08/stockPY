b5bca3b0ed8f88a0e4bc6e8cff35ac745ecd7b22
Vinh <ngoquyvinh05@gmail.com>
2026-06-24 13:55:34 +0700

    update

--- business_data/etl/pipeline.py
+++ business_data/etl/pipeline.py
@@ -1,4 +1,4 @@
-
+import logging
 from dotenv import load_dotenv
 from business_data.etl.crawler.fetch_stock import fetch_stock
 from business_data.etl.transform.clean_data import clean_data
@@ -6,6 +6,18 @@ from business_data.etl.load.load_data import load_data
 
 load_dotenv()
 
-raw_data = fetch_stock()
-cleaned_data = clean_data(raw_data)
-load_data(cleaned_data)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def run():
+    logging.info("bat dau lay dlieu")
+    raw_data = fetch_stock()
+    logging.info("lam sach")
+    cleaned_data = clean_data(raw_data)
+    logging.info("luu")
+    load_data(cleaned_data)
+    logging.info("done")
+if __name__ =="__main__":
+    run()
